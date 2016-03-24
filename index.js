d1fb806593ba70157b76d606562af410efc93075
J.P. Wesselink <jpwesselink@gmail.com>
2016-03-24 20:08:17 +0100

    Verification of existence against npm, domain registration at godaddy

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 
+// moar modules!
+var chalk = require('chalk');
 var crypto = require('crypto');
+var http = require('http');
+var inquirer = require('inquirer');
+var open = require('open');
 
 function random() {
 	var length = 64;
@@ -16,4 +21,30 @@ function random() {
 	return value.join('');
 }
 
-console.log(random());
+function checkIfNameExists() {
+	var name = random();
+	http.request({
+			method: 'HEAD',
+			host: 'registry.npmjs.org',
+			path: '/' + name
+		}, function (r) {
+			if (r.statusCode !== 404) {
+				checkIfNameExists();
+			} else {
+				console.log('Created a beautiful unique available package name for you');
+				console.log(chalk.red.bold(name));
+				inquirer.prompt([{
+					type: 'confirm',
+					name: 'registerDomain',
+					message: 'Would you like to open a browser and for check availability\nof domain names for your package on godaddy.com ?'
+				}], function (answers) {
+					if (answers.registerDomain) {
+						open('https://godaddy.com/domains/searchresults.aspx?checkAvail=1&tld=.tech&domainToCheck=' + name);
+					}
+				});
+			}
+		})
+		.end();
+}
+
+checkIfNameExists();
